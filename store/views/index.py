b2fd13d7cdd7f608f6c17bf9a9eb3be1010ffaa8
Dhruv <Dhruvkhandelwal36@gmail.com>
2025-01-22 18:49:41 +0530

    set pagination on index and orders page

--- store/views/index.py
+++ store/views/index.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.core.paginator import Paginator
 from store.models import Product, Categories
 
 class Index(View):
@@ -13,11 +14,25 @@ class Index(View):
             products = Product.objects.filter(category=category_id)
         else: # when all category is selected
             products = Product.objects.all()
+        
+        #writing pagination logic
+        index_page_pagination = Paginator(products,3)
+        current_page_number = request.GET.get('page')
+        final_pages=index_page_pagination.get_page(current_page_number)
+
+        last_page = index_page_pagination.num_pages
+
+        current_page = final_pages.number
+        start_page = max(1,current_page-1)
+        end_page=min(start_page+2,last_page)
+        list_of_page_numbers= range(start_page,end_page+1)
 
         context={
-            'products':products,
+            'products':final_pages,
+            'last_page':last_page,            
+            'list_of_page_numbers':list_of_page_numbers,
             'categories':categories,
-            'cart': cart
+            'cart': cart,
         }
         # print(request.session.get('session_id')," ", request.session.get('session_email') )
         return render(request,'index.html',context)
--- store/views/orders.py
+++ store/views/orders.py
@@ -1,12 +1,30 @@
 from django.views import View
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from ..models import Orders
 
 class OrdersView(View):
     def get(self,request):
         session_customer_id = request.session.get('session_customer_id')       
         orders_of_customer = Orders.objects.filter(customer_id=session_customer_id).order_by('-date')
+        
+        #code for setting pagination    
+        orders_page_pagination = Paginator(orders_of_customer,1)
+        page_number = request.GET.get('page')
+        final_pages = orders_page_pagination.get_page(page_number)
+        #for sending last page
+        last_page = orders_page_pagination.num_pages
+        #just deriving get request page number from final_pages keys
+        current_page = final_pages.number
+
+        #writing logic to send before and after page to current page
+        start_page = max(current_page - 1, 1)  # Start range (at least page 1)
+        end_page = min(start_page + 2, last_page)  # End range (at most last page)
+        list_of_page_numbers = range(start_page, end_page + 1)
+
         context={
-            'orders':orders_of_customer,
+            'orders':final_pages,
+            'last_page':last_page,
+            'list_of_page_numbers':list_of_page_numbers
         }
         return render(request,'order.html',context)
