29b70cc1b2bd625a778da6d5488feca400f9628c
Dhruv <Dhruvkhandelwal36@gmail.com>
2025-01-24 19:03:07 +0530

    added debug toolbar

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -12,16 +12,23 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+# django message settings
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SITE_ID =1 
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_dmkwx4xmcj9zr6ghk2gg6@!1*2t)dvup435q4#4fuy-h^(#m6'
+SECRET_KEY = "django-insecure-_dmkwx4xmcj9zr6ghk2gg6@!1*2t)dvup435q4#4fuy-h^(#m6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,54 +39,58 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'store',
-    'django.contrib.sites',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "debug_toolbar",
+    "store",
+    "django.contrib.sites",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'Eshop.urls'
+ROOT_URLCONF = "Eshop.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates',], # holds the address of global html templates
-        'APP_DIRS': True, # Tells Django to keep look inside an app's static folder
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],  # holds the address of global html templates
+        "APP_DIRS": True,  # Tells Django to keep look inside an app's static folder
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Eshop.wsgi.application'
+WSGI_APPLICATION = "Eshop.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -89,16 +100,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -106,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -118,35 +129,40 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'  
-''' this gets prepends in front of url requesting static files, can be replaced with CDN link if CDN is serving static
-files, helps server to distinguish static request and avoid naming conflict '''
+STATIC_URL = "static/"
+""" this gets prepends in front of url requesting static files, can be replaced with CDN link if CDN is serving static
+files, helps server to distinguish static request and avoid naming conflict """
 
-STATIC_ROOT = BASE_DIR/"production_files"  
-'''when we run collectstatic command all the static resources (image,logo,css-> everything which remain same throught 
+STATIC_ROOT = BASE_DIR / "production_files"
+"""when we run collectstatic command all the static resources (image,logo,css-> everything which remain same throught 
 the website) accross the project (including Global and inside app static files), will get store at this location. 
 (folder must be created before running command) 
 
 At development level django itself serve static files but in production level server like apache will serve static 
-files form this location'''
+files form this location"""
 
-STATICFILES_DIRS = [ # this is set for django to know that there exist a static folder at global level
-    BASE_DIR/"static"
+STATICFILES_DIRS = [  # this is set for django to know that there exist a static folder at global level
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # settings for media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # setting for automatic mail
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587 # for local system 465 (for live website)
-EMAIL_HOST_USER = 'dhruvdotsquares@gmail.com' 
-EMAIL_HOST_PASSWORD = 'tbkjkzqcpwyxsfll '
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587  # for local system 465 (for live website)
+EMAIL_HOST_USER = "dhruvdotsquares@gmail.com"
+EMAIL_HOST_PASSWORD = "tbkjkzqcpwyxsfll "
 EMAIL_USE_TLS = True
+
+# setting internal ip for toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
